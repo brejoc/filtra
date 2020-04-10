ff315254271350c5c93fbb4ae664c39baf55bd94
Joao Cavalheiro <jcavalheiro@suse.de>
2020-04-10 18:49:17 +0100

    Fix unit tests

--- metrics_test.go
+++ metrics_test.go
@@ -1,32 +1,44 @@
 package main
 
 import (
-	"testing"
-
 	"github.com/brejoc/filtra/persist"
 	log "github.com/sirupsen/logrus"
+	"reflect"
+	"testing"
 )
 
 func TestNewMetrics(t *testing.T) {
+	// loading test config
+	loadConfig("./test-data/test_config.toml")
+
 	var results QueryPages
 	if err := persist.Load("./test-data/query_pages.dump", &results); err != nil {
 		log.Fatal(err)
 	}
 	got := NewMetrics(&results)
-	want := GithubMetrics{
-		allIssuesCounter:    14,
-		closedIssueCounter:  5,
-		openIssueCounter:    9,
-		openBugsCounter:     1,
-		openL3Counter:       1,
+
+	testBoard := map[string]*BoardMetrics{"test": &BoardMetrics{
+		closedIssueCounter:  4,
+		openIssueCounter:    8,
 		blockedIssueCounter: 3,
 		plannedIssueCounter: 3,
-		averageLeadTime:     312.8581111111111,
-		averageCycleTime:    57.33476851851852,
+		openBugsCounter:     1,
+		openL3Counter:       1,
+		averageLeadTime:     165.7965451388889,
+		averageCycleTime:    148.83974247685185,
+	}}
+
+	want := GithubMetrics{
+		closedIssueCounter: 5,
+		openIssueCounter:   9,
+		openBugsCounter:    1,
+		openL3Counter:      2,
+		Board:              testBoard,
 	}
-	if want != got {
-		t.Logf("Got this:    %v", got)
-		t.Logf("Wanted this: %v", want)
+
+	if !reflect.DeepEqual(want, got) {
+		t.Logf("Got this:    %v %v\n", got, got.Board["test"])
+		t.Logf("Wanted this: %v %v\n", want, want.Board["test"])
 		t.Error("Metrics are not matching.")
 	}
 }
